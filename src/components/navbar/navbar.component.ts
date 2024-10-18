ebcf65aab0e21f87cd0236d453b7f03af81abe48
nikhil <001nikhilshirsath001@gmail.com>
2024-10-18 19:14:38 +0530

    routing added

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private route :Router){
+
+  }
+  callLoginPage(){
+    console.log("login clicked");
+    this.route.navigateByUrl('loginPage');
+  }
+
+  callSignupPage(){
+    console.log("signup clicked");
+    this.route.navigateByUrl("signupPage");
+  }
 
 }
--- src/components/porfolio/porfolio.component.ts
+++ src/components/porfolio/porfolio.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-porfolio',
+  selector: 'app-portfolio',
   standalone: true,
   imports: [],
   templateUrl: './porfolio.component.html',
   styleUrl: './porfolio.component.css'
 })
-export class PorfolioComponent {
+export class PortfolioComponent {
 
 }
