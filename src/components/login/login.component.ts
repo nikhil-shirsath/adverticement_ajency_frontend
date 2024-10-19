61b5de2c98e4d0ab13676aff63cfcd198b14f514
nikhil <001nikhilshirsath001@gmail.com>
2024-10-19 16:24:33 +0530

    login validataon

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,15 +1,36 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import {  ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import {  FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,RouterLink],
+  imports: [ReactiveFormsModule,RouterLink,JsonPipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginData  :FormGroup =  new FormGroup({
+    userEmail:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(40),Validators.email]),
+    userPassword: new FormControl('',[Validators.required]),
+    isChecked: new FormControl(false,[Validators.required]),
+    userRole:new FormControl()
+
+  });
+
+  LoginFormValue:any;
+
+  constructor(private route: Router){
+
+  }
+  redirectToSignUp(){
+    this.route.navigateByUrl("signupPage");
+  }
+
+  onSubmit(){
+    this.LoginFormValue= this.loginData.value;
+  }
 
 }
