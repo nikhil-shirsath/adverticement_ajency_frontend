4ebfc4274ef377d0bc5cc48cc82e146682cdbf46
nikhil <001nikhilshirsath001@gmail.com>
2024-10-19 17:41:02 +0530

    signup page validation

--- src/components/signup/signup.component.ts
+++ src/components/signup/signup.component.ts
@@ -1,12 +1,40 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,JsonPipe],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  
+  states:string[]=['maharashtra','goa','mp','chattisgadh'];
+  registerUser:FormGroup= new FormGroup(
+    {
+      userFirstName: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(40)]),
+      userLastName: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(40)]),
+      userEmail: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(40),Validators.email]),
+      userPassword: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(40)]),
+      userConfirmPassword: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(40)]),
+      userMobileNo: new FormControl('',[Validators.required,]),
+      userAddress: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(100)]),
+      userCity: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+      userState: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+      userPinCode: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(7)]),
+      userIsAcceptTerms: new FormControl('',[Validators.required]),
+      
+    }
+  )
+
+  signUpFormData :any;
+
+  onSignUp(){
+    this.signUpFormData = this.registerUser.value;
+    console.log(this.signUpFormData);
+  }
+
 
 }
