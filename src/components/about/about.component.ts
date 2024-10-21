5ab43b28c37ca2e55dcd41ecc57b8cd579f49043
nikhil <001nikhilshirsath001@gmail.com>
2024-10-21 16:20:04 +0530

    about section code

--- src/components/about/about.component.ts
+++ src/components/about/about.component.ts
@@ -9,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  aboutContent:string ="About Malhar Advertisers At Malhar Advertisers, we believe that every brand has a story waiting to be told. Founded on the principles of creativity, innovation, and strategy, we are a dynamic advertising agency dedicated to elevating your brand’s presence in a crowded marketplace. Our team of passionate professionals combines artistic flair with analytical insights, crafting compelling campaigns that resonate with your target audience. Whether you’re a startup looking to make your mark or an established brand seeking a fresh perspective, we tailor our services to meet your unique needs."
+  serviceContent:any[]=[{
+    heading:"Our Mission:",
+    content:["To empower brands through innovative advertising solutions that drive engagement, build loyalty, and foster growth.",'hii']
+  },{
+    heading:"Our Services:",
+    content:["From digital marketing and social media management to branding and traditional advertising, we offer a comprehensive suite of services designed to enhance your brand’s visibility and impact."]
+  }
+]
+
 }
